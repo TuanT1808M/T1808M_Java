c74a42f14cf3345503e9748f2af8e543c34c2a73
TuanT1808M <46362124+TuanT1808M@users.noreply.github.com>
2019-05-03 23:03:44 +0700

    JV2_Session2

--- Main.java
+++ Main.java
@@ -1,24 +1,33 @@
-package sample;
+package JV2_Session2;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.util.ArrayList;
 
-public class Main extends Application {
-    public static Stage mainStage;
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        mainStage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-    }
-
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        plus(5,7,4,5);
+        plus("aaa","bbb",6,7);
+        MyGeneric<String> myGeneric=new MyGeneric<>();
+
+
     }
+    public static <E,X,Y> void plus(E a,E b,X x,Y y){
+        if (a instanceof Integer){
+            Integer k=(Integer)a+(Integer)b;
+            System.out.println(k);
+            //return a;
+        }
+        else if (a instanceof Double){
+            Double k=(Double)a+(Double)b;
+            System.out.println(k);
+            //return a;
+        }
+        else {
+            System.out.println("Khong tinh toan dc \nThao ngu .");
+            //  return null;
+        }
+    }
+    public static <T> T changeValue(T obj){
+        return obj;
+    }
+
 }
