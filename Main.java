d55ceac6e9f867692041a4798763759cbef9705b
TuanT1808M <46362124+TuanT1808M@users.noreply.github.com>
2019-05-03 23:19:08 +0700

    Lab_ss3

--- Main.java
+++ Main.java
@@ -1,22 +1,13 @@
-package Assigment2;
-import  java.lang.String;
+package LabSession3;
+
 public class Main {
+
     public static void main(String[] args) {
-        Student hocsinh1 = new Student("TH101","Thao",20);
-        Student hocsinh2 = new Student("TH102","Quan",19);
 
-        PersonModel<Student> lists1 = new PersonModel<>();
-        lists1.addInfo(hocsinh1);
-        lists1.addInfo(hocsinh2);
-        lists1.showInfo();
-
-        Employee nhanvien1 = new Employee("MD296","Ngoc",2500.01);
-        Employee nhanvien2 = new Employee("MD410","Truong",3000.0);
-
-
-        PersonModel<Employee> lists2 = new PersonModel<>();
-        lists2.addInfo(nhanvien1);
-        lists2.addInfo(nhanvien2);
-        lists2.showInfo();
+        thread s1=new thread();
+        s1.setName("MyjavaThread");
+//        s1.start();
+        SubThread s2=new SubThread();
+        s2.start();
     }
 }
